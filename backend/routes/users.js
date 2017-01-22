49f83227ca0ca638bd7ff58a3e088119f648e0c7
Immo Struchholz <mrerody@googlemail.com>
2017-01-22 18:24:47 +0100

    save users in mongo database

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import validateInput from '../shared/validations/signup';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
@@ -7,9 +8,17 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
 	const { errors, isValid } = validateInput(req.body);
+	const db = req.db;
+	const collection = db.get('usersCollection');
 
 	if(isValid) {
-		res.json({ success: true });
+		const { username, email, password, timezone } = req.body;
+		const password_digest = bcrypt.hashSync(password, 10);
+		const timestamp = new Date().getTime(); // miliseconds
+		collection.insert({ username, email, password_digest, timezone, timestamp })
+			.then(x => res.json({ success: true }))
+			.catch(err => res.status(500).json({ error: err }));
+
 	} else {
 		res.status(400).json(errors);
 	}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import mongodb from 'mongodb';
+import monk from 'monk';
 import bodyParser from 'body-parser';
 
 import users from './routes/users';
 
 const app = express();
-
 app.use(bodyParser.json());
+// need to start mongodb first for this to work
+const db = monk('localhost:27017/reddice');
 
+app.use((req, res, next) => {
+	req.db = db;
+	next();
+});
 app.use('/api/users', users);
 
 app.listen(8080, () => console.log('Listening on localhost:8080'));
